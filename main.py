dc507a330a96c9446b4a8db29076a6aec2905a18
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2023-12-05 14:27:10 +0000

    Added testing DB connection

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 Main App
 """
 
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+from peewee import PostgresqlDatabase
 
 app = FastAPI()
+load_dotenv()
+
+# Testing DB connection
+db = PostgresqlDatabase(
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
 
 
 @app.get("/health-check/")
