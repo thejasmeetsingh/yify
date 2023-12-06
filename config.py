03556d6fbdf4ddc205b721527beba7aedf2e2584
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-12-06 13:27:16 +0530

    Added python for centrializing env values

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
